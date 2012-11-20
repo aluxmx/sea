c8ee55563c62db361c7e945c4454350ebeab51f2
Carlos Monterrosa <cmonterrosa@gmail.com>
2012-11-20 15:28:52 -0600

    + Background method for show a color for the zone of sitting

--- app/helpers/invitados_helper.rb
+++ app/helpers/invitados_helper.rb
@@ -1,2 +1,11 @@
 module InvitadosHelper
+def background(registro)
+    color = {"ROJA" => "Ce365a",
+             "VERDE" => "96cb5d",
+             "AZUL" => "3c5ba9"
+    }
+    return "<th width='22px' style='background-color: ##{color[registro.zona.descripcion]}'></th>"
+end
+
+
 end
