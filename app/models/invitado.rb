1cb8e0eddde813dcac01464019b29a4191988e2c
Carlos Monterrosa <cmonterrosa@gmail.com>
2012-11-20 15:29:31 -0600

    + Methods for show the current and the next state of the row

--- app/models/invitado.rb
+++ app/models/invitado.rb
@@ -1,2 +1,24 @@
 class Invitado < ActiveRecord::Base
+  has_many :bitacoras
+  belongs_to :zona
+
+  def ultimo_estado
+     return @last_log = Bitacora.find(:first, :conditions => ["invitado_id = ?", self.id], :order => "updated_at DESC")
+  end
+
+  def siguiente_estado
+     if @last_log = Bitacora.find(:first, :conditions => ["invitado_id = ?", self.id], :order => "updated_at DESC")
+          if Estado.exists?(@last_log.estado_id + 1)
+            return Estado.find(@last_log.estado_id + 1)
+          end
+     end
+     return nil
+  end
+
+  def before_save
+    self.nombre.upcase!
+    self.cargo.upcase!
+    self.direccion.upcase!
+  end
+
 end
