88be8c54d2fa7698b49dde7c72151488c8159ccc
Carlos Monterrosa <cmonterrosa@gmail.com>
2012-11-20 15:34:12 -0600

    + In migration file remove full name and add a cargo field

--- db/migrate/20121114171130_create_invitados.rb
+++ db/migrate/20121114171130_create_invitados.rb
@@ -2,13 +2,11 @@ class CreateInvitados < ActiveRecord::Migration
   def self.up
     create_table :invitados do |t|
       t.integer :folio
-      t.string :paterno
-      t.string :materno
-      t.string :nombre
+      t.string :nombre, :limit => 140
+      t.string :cargo, :limit => 120
       t.string :telefono
       t.string :direccion
       t.integer :zona_id
-
       t.timestamps
     end
   end
